78f56d45b3f21679e9d63301e90a80a2f9400a7a
Arsen Galiev <1234xl.com@gmail.com>
2026-04-20 16:28:16 +0300

    fix: add nitro for tanstack start deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { nitro } from 'nitro/vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
@@ -9,11 +9,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   plugins: [
-    devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro()
   ],
 //  server: {
 //    host: '0.0.0.0',
